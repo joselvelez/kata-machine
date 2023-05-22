56260472c52b374ce9279ba1403e2d108114160b
joselvelez <joselvelez@protonmail.com>
2023-05-22 05:32:48 -0500

    update

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,9 @@
 export default function bubble_sort(arr: number[]): void {
-
+    for (let i = 0; i <= arr.length; i++) {
+        if (arr[i] > arr[i+1]) {
+            let beg = arr.slice(0, i);
+            let end = arr.slice(i+1, -1);
+            let newArray = beg.concat(i+1, i, end);
+        }
+    }
 }
